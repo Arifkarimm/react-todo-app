53000d644a41dee8bc8ce95ead962e6beea8bc55
Fazlul <fkarif.cse@gmail.com>
2019-09-13 16:48:43 +0200

    singletodo list break

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 
 
+const SingleTodoList = ({todoItem, deleteItem}) => {
+	return(
+		<li key={todoItem.key}>
+							{todoItem.text}
+							<button type="text" onClick={() => deleteItem(todoItem.key)}>Remove</button>
+						</li>
+	)
+}
+
 class TodoList extends Component {
 	render() {
 		return (
 			<div className="todList">
 				<ul className="todList-list">
 					{this.props.entries.map(item => (
-						<li key={item.key}>
-							{item.text}
-							<button type="text" onClick={() => this.props.deleteItem(item.key)}>Remove</button>
-						</li>
+						<SingleTodoList key={item.key} todoItem={item} deleteItem={this.props.deleteItem} />
 					))}
 				</ul>
 			</div>
